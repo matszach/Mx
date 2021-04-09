9ce59999aa691fb3d8bd4cea0f30dca5d37fd821
matszach <lkaszubowski@gmail.com>
2021-04-09 19:47:43 +0200

    init

---
+++ mx.js
@@ -0,0 +1,11 @@
+/**
+ * Collection of tools that can be used to create games  with JS and HTML5 canvas
+ * @author Lukasz Kaszubowski
+ * @see https://github.com/matszach
+ * @version 0.0.1
+ */
+const MX = {
+
+    
+
+}
