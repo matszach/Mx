193e530d1d1d9436f9e3b84451da550d9680cf82
matszach <lkaszubowski@gmail.com>
2021-04-10 16:10:34 +0200

    loop

--- mx.js
+++ mx.js
@@ -3,9 +3,121 @@
  * Collection of tools that can be used to create games  with JS and HTML5 canvas
  * @author Lukasz Kaszubowski (matszach)
  * @see https://github.com/matszach
- * @version 0.1.1
+ * @version 0.2.0
  */
- const Mx = {
+
+/** ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+ * Base classes extended within the library
+ */
+
+/** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+ * Base entity class, extended by geometry classes and by image draw classes
+ */
+class _Entity {
+
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    place(x, y) {
+        this.x = x;
+        this.y = y;    
+        return this;       
+    }
+
+    move(x, y) {
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+
+    movePolar(phi, r) {
+        // TODO
+        return this;
+    }
+    
+    scale(xScale, yStale, xOrigin = this.x, yOrigin = this.y) {
+        // abstract
+        return this;
+    }
+
+    rotate(phi, xOrigin = this.x, yOrigin = this.y) {
+        // abstract
+        return this;
+    }
+
+    _getDrawn(canvasHandler) {
+        // abstract
+    }
+
+
+}
+
+/** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+ * Library body proper
+ */
+const Mx = {
+
+    /** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+     * Assigned here for public access
+     */
+    Entity: _Entity,
+
+    /** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+     * Entity that can contain other entities
+     */
+    Container: class extends _Entity {
+
+        constructor(x, y) {
+            super(x, y);
+            this.children = [];
+        }
+
+        add(entity) {
+            this.children.push(entity);
+        }
+
+        forChild(callback) {
+            this.children.forEach(callback);
+        }
+
+        place(x, y) {
+            const dx = x - this.x;
+            const dy = y - this.y;
+            this.x = x;
+            this.y = y;
+            this.forChild(c => c.move(dx, dy));
+        }
+    
+        move(x, y) {
+            super.move(x, y);
+            this.forChild(c => c.move(x, y));
+            return this;
+        }
+    
+        movePolar(phi, r) {
+            super.movePolar(phi, r);
+            this.forChild(c => c.movePolar(phi, r));
+            return this;
+        }
+        
+        scale(xScale, yStale, xOrigin = this.x, yOrigin = this.y) {
+            super.scale(xScale, yStale, xOrigin, yOrigin);
+            this.forChild(c => c.scale(xScale, yStale, xOrigin, yOrigin));
+            return this;
+        }
+    
+        rotate(phi, xOrigin = this.x, yOrigin = this.y) {
+            // abstracta
+            return this;
+        }
+    
+        _getDrawn(canvasHandler) {
+            this.forChild(c => this._getDrawn(canvasHandler))
+        }
+    
+    },
 
     /** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
      * Random number generator
@@ -15,11 +127,6 @@
         static _MIN_CHAR_CODE = 33;
         static _MAX_CHAR_CODE = 127;
 
-        /**
-         * TODO
-         * @param {any} seed 
-         * @returns 
-         */
         static _transformSeed(seed) {
             const stringified = JSON.stringify(seed);
             const split = stringified.split('');
@@ -28,10 +135,6 @@
             return absolute;
         }
 
-        /**
-         * Generates a random 20 character seed
-         * @returns {string} a generated seed
-         */
         _generateRandomSeed() {
             let seed = '';
             for(let i = 0; i < 20; i++) {
@@ -41,9 +144,6 @@
             return seed;
         }
 
-        /**
-         * TODO
-         */
         _initRandomGeneratorIndices() {
             this._indices = [];
             for(let i = 0; i < this.precision; i++) {
@@ -51,10 +151,6 @@
             }
         }
 
-        /**
-         * TODO
-         * @returns 
-         */
         _random() {
             let counter = '';
             for(let i = 0; i < this.precision; i++) {
@@ -188,6 +284,90 @@
             return arr;
         }
 
+    },
+
+    /** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+     * Iteration tools
+     */
+    It: {
+
+        /**
+         * TODO
+         * @param {number} times 
+         * @param {Function} callback 
+         */
+        times(times, callback) {
+            for(let i = 0; i < times; i++) {
+                callback(i, times);
+            }
+        },
+
+
+        /**
+         * Interval wrapper
+         */
+        Loop: class {
+
+            static start(tps, callback) {
+                return new Mx.It.Loop(tps, callback).start();
+            }
+
+            constructor(tps, callback) {
+                this.tps = tps;
+                this.tickCount = 0;
+                this._interval; 
+                this._callback = callback;
+            }
+
+            start() {
+                this._interval = setInterval(() => {
+                    this.tickCount++;
+                    this.callback(this);
+                });
+                return this;
+            }
+
+            stop() {
+                clearInterval(this._interval);
+                this._interval = null;
+                return this;
+            }
+
+        }
+
+    },
+
+    /** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+     * Geometry entities and tools
+     */
+    Geo: {
+
+        Vertex: class extends _Entity {
+
+            toCircle(radius) {
+                // TODO
+            }
+
+            _getDrawn(canvasHandler) {
+                // TODO
+            }
+
+        }
+
+    },
+
+    /** ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+     * Drawing and canvas handling classes and tools
+     */
+    Draw: {
+
+        CanvasHandler: class {
+
+            static init(homeId) {
+
+            }
+
+        }
     },
 
 }
