0a5174ae9364cfec41366e05fd8051eb5cc200e3
matszach <lkaszubowski@gmail.com>
2021-04-25 16:23:15 +0200

    +

--- mx.js
+++ mx.js
@@ -3,7 +3,7 @@
  * Collection of tools that can be used to create games  with JS and HTML5 canvas
  * @author Lukasz Kaszubowski (matszach)
  * @see https://github.com/matszach
- * @version 0.10.4
+ * @version 0.11.0
  */
 
 /** ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
